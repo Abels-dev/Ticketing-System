f60169b2f484be19b96557f630fad10ce33e71cf
abel <abelgetachew143@gmail.com>
2025-03-07 00:47:14 +0300

    fixed minor issues in the user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import Joi from "joi";
 const userSchema = new mongoose.Schema({
    name: {
       type: String,
@@ -30,15 +31,26 @@ userSchema.pre("save", async function (next) {
    if (!this.isModified("password")) {
       next();
    }
-   this.password = await bcrypt.hash(this.password, 10);
+   try {
+      this.password = await bcrypt.hash(this.password, 10);
+      next();
+   } catch (error) {
+      next(error);
+   }
 });
 
-userSchema.methods(
-   (matchPassword = async (enteredPassword) => {
-      return await bcrypt.compare(enteredPassword, this.password);
-   })
-);
+userSchema.methods.comparePassword = async function (password) {
+   return await bcrypt.compare(password, this.password);
 
+};
+export const validateUser = (user) => {
+   const schema = Joi.object({
+      name: Joi.string().required().min(3),
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(6),
+   });
+   return schema.validate(user);
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
