394c1a4a989cb73f42dbbf588b7a695b69f66dbc
abel <abelgetachew143@gmail.com>
2025-03-19 22:01:13 +0300

    made some fix on the backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 export const protectRoute=async (req,res,next)=>{
       try {
-           const token=req.cookies.token;
+           const token=req.cookies.ticket_token;
            if(!token){
                 return res.status(401).json({success:false,message:"No token found, authorization denied"})
            }
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,7 +5,7 @@ export const generateTokenAndSetCookie=(user,res)=>{
       const token=jwt.sign({id:user._id},ENV_VARS.JWT_SECRET,{
         expiresIn:"15d"
       })
-        res.cookie('token',token,{
+        res.cookie('ticket_token',token,{
             httpOnly:true,
             maxAge:15*24*60*60*1000,
             secure:ENV_VARS.NODE_ENV==='production',
