b1beb8a2633685e9da0d746c02fe1f9f32c740ec
abel <abelgetachew143@gmail.com>
2025-03-19 22:01:53 +0300

    structured the react routes in the app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+import { SignupPage } from "./pages/SignupPage"
+import {LoginPage} from './pages/LoginPage'
+import {Toaster} from 'react-hot-toast'
+import { useAuthStore } from "./store/authStore"
+import { useEffect } from "react"
+function App() {
+  const {user,checkAuth,isCheckingAuth}=useAuthStore();
+  useEffect(()=>{
+      checkAuth();
+  },[checkAuth])
+  if(isCheckingAuth){
+    return (
+        <div className="h-screen bg-slate-400 w-full flex justify-center items-center">
+           <div className="size-10 border-dotted border-4 border-cyan-950 rounded-full animate-spin"></div>
+        </div>
+    )
+  }
+  return (
+    <>
+       <Routes>
+          <Route path="/" element={user?<HomePage/>:<Navigate to={"/signup"}/>}/>
+          <Route path="/signup" element={(!user)?<SignupPage/>:<Navigate to={"/"}/>}/>
+          <Route path="/login" element={(!user)?<LoginPage/>:<Navigate to={"/"}/>}/>
+       </Routes>
+       <Toaster/>
+    </>
+  )
+}
+
+export default App
