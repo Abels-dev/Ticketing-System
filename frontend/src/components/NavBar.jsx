899c985de944e17ba031d3ab0d392296ae4c0a4b
abel <abelgetachew143@gmail.com>
2025-03-21 11:48:44 +0300

    created the navbar componenet

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import {LogOut} from 'lucide-react'
+import { useAuthStore } from "../store/authStore"
+export const NavBar = () => {
+  const {user,logout}=useAuthStore();
+  return (
+
+    <div className="p-5 mx-auto max-w-6xl flex items-center justify-between bg-slate-100 text-slate-900">
+        <h1 className="font-bold text-3xl text-cyan-900">Ticket</h1>
+        <nav className="flex items-center gap-8 font-bold">
+            <Link to={'/'} className="hover:underline hover:text-slate-600">Home</Link>
+            <Link to={'/create'} className="hover:underline hover:text-slate-600">CreateTicket</Link>
+            <Link to={'/about'} className="hover:underline hover:text-slate-600">About</Link>
+            <div className="flex items-center gap-2">
+            <LogOut className="size-8 cursor-pointer" onClick={()=>logout()}/>
+            <div className="size-10 rounded-full bg-red-800
+             text-slate-300 font-bold text-xl flex items-center justify-center capitalize">{user.name[0]}</div>
+            </div>
+        </nav>
+    </div>
+  )
+}
