9fdab43fa928113eaeef7713c0eaa80cb91d8828
abel <abelgetachew143@gmail.com>
2025-03-21 11:51:34 +0300

    added the navbar to the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
-import { useAuthStore } from "../store/authStore"
+import { NavBar } from "../components/NavBar";
 
 export const HomePage = () => {
-  const {logout}=useAuthStore();
   return (
-    <div className=" flex items-center justify-between p-6">
-      <div className="m-5 text-3xl text-red-500 p-4 font-bold">HomePage</div>
-      <button className="p-2 rounded bg-cyan-900 text-slate-300 font-bold mr-4" onClick={logout}>Logout</button>
+    <div className="h-screen bg-slate-100">
+      <NavBar/>
     </div>
   )
 }
